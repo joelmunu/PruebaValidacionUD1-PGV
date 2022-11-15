3e91483b42429ce54621f09a14c36ed834642012
joelmunu <joel.munu@gmail.com>
2022-11-15 10:19:37 +0000

    Style(main.py): Se han añadido varios saltos de línea para mejorar la legibilidad de la salida del programa por consola

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ def padre():
         if newPid == 0:
             hijo()
         else:
-            print('Iniciando el proceso:', newPid, 'a las', time.strftime('%H:%M:%S', time.localtime()))
+            print('\nIniciando el proceso:', newPid, 'a las', time.strftime('%H:%M:%S', time.localtime()))
 
         sleep(10)
                 
 def hijo():
-    print('Iniciado el proceso con PID:', os.getpid())
+    print('\nIniciado el proceso con PID:', os.getpid())
     sleep(3)
-    print('Terminando el proceso con PID:', os.getpid())
+    print('\nTerminando el proceso con PID:', os.getpid(), '\n')
     os._exit(0)
     
 if __name__ == '__main__':
